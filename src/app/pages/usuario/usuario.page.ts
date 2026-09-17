962a41b98f19ceb4d51dc6c736f6c01c065e4b4d
victorhmsdev <victorhugomendesoares@gmail.com>
2026-09-17 16:53:16 -0300

    correção no usuario

--- src/app/pages/usuario/usuario.page.ts
+++ src/app/pages/usuario/usuario.page.ts
@@ -188,6 +188,8 @@ export class UsuarioPage implements OnInit {
 
       if (novaSenha) {
         this.usuario.senha = novaSenha;
+      } else {
+        this.usuario.senha = null as any;
       }
 
       const salvarDadosUsuario = () => this.usuarioService.salvar(this.usuario);
